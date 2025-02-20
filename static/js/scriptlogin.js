e0db0d1c3dd41653fd9bdaac21f4d4cb90a186e8
gnobisP <gustavopimenta90@gmail.com>
2025-02-20 03:12:42 -0300

    concertado PB login

--- static/js/scriptlogin.js
+++ static/js/scriptlogin.js
@@ -1,50 +1,18 @@
 async function checklogin() {
     try {
-        
         const response = await fetch('/checklogin');
-        
         const usuarios = await response.json();
         console.log('Dados recebidos:', usuarios);
-        alert("❌ Usuário ou senha incorretos.");
 
         const email = document.getElementById("email").value.trim();
         const password = document.getElementById("password").value.trim();
 
-        /*let correto = false;
-        
-        for (let user of usuarios) {
-            const str1 = JSON.stringify(user);
-            if (str1.includes(email) && str1.includes(password)) {
-                correto = true;
-                break;
-            } else {
-                correto = false;
-            }
-        }
-
-        if (correto) {
-            window.location.href = "/AreaUsuario";
-        } else {
-            alert("❌ Usuário ou senha incorretos.");
-        }*/
-
-        
-
-        
-
         let correto = false;
         for (let user of usuarios) {
-            alert("User:" + user);
-            const usuario = {email: email, password: password}
-            console.log()
-            //const str1 = JSON.stringify(user);
-            if (usuario.email == user.email) {
-                alert("Vai se fuder");
+            console.log(password + "   " + user.senha);
+            if (email === user.email && password === user.senha) {
                 correto = true;
                 break;
-            } else {
-                alert("Vai se fuder 2");
-                correto = false;
             }
         }
 
