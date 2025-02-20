5419168ab4b362f7fa8f0c6e13be3396b6cd371b
galliegos <gallegosgabriel35@gmail.com>
2025-02-20 15:26:22 -0300

    style footer

--- static/js/padroes/footer.js
+++ static/js/padroes/footer.js
@@ -4,26 +4,29 @@ document.write(`
             <div class="row">
                 <div class="col-md-4 footer-logo-box">
                     <a href="index.html">
-                        <img src="../assets/img_footer/gcc.png" alt="logo_gcc" />
+                        <img src="../assets/img_footer/LogoRestaurante.jpg" alt="logo_restaurante" />
                     </a>
-                    <h5 class="footer-logo-box">&gt; CEFET-MG_</h5>
+                    <h5 class="footer-logo-box">&gt; Nome do Restaurante_</h5>
                 </div>
-                <div class="col-md-6"><!--FUTURO--></div>
-                <div class="col-md-2 footer-icons-social-media">
-                    <a href="https://www.instagram.com/gcc.cefetmg?utm_source=qr" id="instagram" target="_blank"
+                <div class="col-md-6 text-white text-center">
+                    <p>O melhor Restaurante da região!</p>
+                    <p>Telefone: (31) 976823732</p>
+                    <p>Horário de funcionamento: 08h - 23h</p>
+                </div>
+                <div class="col-md-2 footer-icons-social-media text-end">
+                    <a href="https://www.instagram.com/seu_restaurante" id="instagram" target="_blank"
                         rel="external">
                         <i class="fab fa-instagram fa-2x" style="color: #ffffff"></i>
                     </a>
-
-                    <a href="https://chat.whatsapp.com/Cm1OKkmDcuV6Y7ki18bSrZ" id="whatsapp" target="_blank"
+                    <a href="https://wa.me/seu_numero" id="whatsapp" target="_blank"
                         rel="external">
                         <i class="fab fa-whatsapp fa-2x" style="color: #ffffff"></i>
                     </a>
-                    <a href="mailto:gcccefetmg@gmail.com" id="email">
+                    <a href="mailto:contato@seurestaurante.com" id="email">
                         <i class="far fa-envelope fa-2x" style="color: #ffffff"></i>
                     </a>
                 </div>
             </div>
         </div>
     </footer>
-  `);
+`);
